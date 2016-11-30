0e90448300a13b23548b72c01a577bcac26ef2f9
Matt Pestridge <mpestridge1986@gmail.com>
2016-11-30 14:38:06 -0700

    completed part 1 and figured out camelize and decamelize using humps

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,34 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const {camelizeKeys, decamelizeKeys} = require('humps');
 
-// YOUR CODE HERE
+router.get('/books', (_req, res, next) => {
+  knex('books')
+    .orderBy('id')
+    .then((books) => {
+      res.send(camelizeKeys(books));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('books/:id', (req, res, next) => {
+  knex('books')
+  .where('id', req.params.id)
+  .first()
+  .then((books) => {
+    if (!books) {
+      return next();
+    }
+    res.send(camelizeKeys(books));
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
 
 module.exports = router;
