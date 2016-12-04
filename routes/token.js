9f92a5bc4eec46f702b81e5b0254c3511e243744
Matt Pestridge <mpestridge1986@gmail.com>
2016-12-04 15:00:45 -0700

    passed part 4 seed test

--- routes/token.js
+++ routes/token.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('/..knex');
+const {camelizeKeys} = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,34 @@
 'use strict';
 
 const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const {camelizeKeys, decamelizeKeys} = require('humps');
+const bcrypt = require('bcrypt-as-promised');
 
 // eslint-disable-next-line new-cap
-const router = express.Router();
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  let decam = decamelizeKeys(req.body);
+  bcrypt.hash(req.body.password, 12)
+  .then((hashed_password) => {
+    return knex('users')
+    .insert({
+        first_name: decam.first_name,
+        last_name: decam.last_name,
+        email: decam.email,
+        hashed_password: hashed_password},
+        '*');
+  })
+    .then((users) => {
+      const user =users[0];
+      delete user.hashed_password;
+      res.set('Content-Type', 'Apllication/json');
+      res.send(camelizeKeys(user));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
